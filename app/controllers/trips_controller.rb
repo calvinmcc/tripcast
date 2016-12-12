5a64659fcdbb66974376b980a2dd0fc452cc2d64
Calvin McConnell <calvin.mcconnell20@gmail.com>
2016-12-11 17:37:46 -0800

    finish implementing basic crud

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,11 +5,10 @@ class TripsController < ApplicationController
 
   def create
     @user = current_user
-    trip_params = params.require(:trip).permit([:start, :end, :departure])
     @trip = Trip.new trip_params
     @trip.user = @user
     if @trip.save
-      redirect_to @trip, notice: 'Trip saved!'
+      redirect_to @trip
     else
       render :new, alert: 'Trip not saved!', id: 'error'
     end
@@ -20,5 +19,27 @@ class TripsController < ApplicationController
     @user = current_user
   end
 
+  def update
+    @user = current_user
+    @trip = Trip.find params[:id]
+    @trip.user = @user
+    if @trip.update trip_params
+      redirect_to @trip, notice: 'Trip updated!'
+    else
+      render :new, alert: 'Trip not saved!', id: 'error'
+    end
+  end
 
+  def destroy
+    @trip = Trip.find params[:id]
+    @user = current_user
+    @trip.destroy
+    redirect_to @user
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit([:start, :end, :departure])
+  end
 end
